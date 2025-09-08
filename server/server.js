77254b061aa95bd4d7863884d0b680f6f6e50502
KIMYESEUL <kimesil1008@ajou.ac.kr>
2025-09-08 18:18:53 +0900

    20250908

--- server/server.js
+++ server/server.js
@@ -61,6 +61,31 @@ app.get('/emp/list', async (req, res) => { // /emp/list: 주소 이름
   }
 });
 
+app.get('/emp/info', async (req, res) => { // /emp/list: 주소 이름
+  const { empNo } = req.query;
+  try {
+    const result = await connection.execute(`SELECT E.*, EMPNO "empNo", ENAME "eName", JOB "job", DEPTNO "selectDept" FROM EMP E WHERE EMPNO = ${empNo}`); // 해당 사번을 가진사람의 정보 리턴
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    // 리턴 (맵 형태로)
+    res.json({ // 코드가 성공적으로 실행됐을때 이 코드를 보내줌
+        result : "success",
+        info : rows[0]
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
 app.get('/emp/delete', async (req, res) => {
   const { empNo } = req.query; // 파라미터 값 받아줌
 
@@ -111,6 +136,24 @@ app.get('/prof/delete', async (req, res) => {
     await connection.execute(
       `DELETE FROM PROFESSOR WHERE PROFNO = :profNo`,
       [profNo], // 변수 사용하는법1. 넣고자 하는 변수 이 리스트에 담고, 그후 :으로 위에서 호출
+      { autoCommit: true } // 변수 사용하는법2. '${}' 사용해서 넣기
+    );
+    res.json({// 코드가 성공적으로 실행됐을때 이 코드를 보내줌
+        result : "success"
+    });
+  } catch (error) {
+    console.error('Error executing insert', error);
+    res.status(500).send('Error executing insert');
+  }
+});
+
+app.get('/emp/insert', async (req, res) => {
+  const { empNo, eName, job, selectDept } = req.query; // 파라미터 값 보내줌
+
+  try {
+    await connection.execute(
+      `INSERT INTO EMP(EMPNO, ENAME, JOB, DEPTNO) VALUES(:empNo, :eName, :job, :selectDept)`,
+      [empNo, eName, job, selectDept], // 변수 사용하는법1. 넣고자 하는 변수 이 리스트에 담고, 그후 :으로 위에서 호출
       { autoCommit: true } // 변수 사용하는법2. '${}' 사용해서 넣기
     );
     res.json({// 코드가 성공적으로 실행됐을때 이 코드를 보내줌
